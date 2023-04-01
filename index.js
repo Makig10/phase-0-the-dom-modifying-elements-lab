4ec34ee42e1903e049b2d84c1f3e32d16d9409b0
Brian <mahuibrian14@gmail.com>
2023-04-01 13:28:50 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//const body=document.querySelector('body');
+const main=document.getElementById('main');
+main.remove();
+const newHeader=document.createElement('h1');
+newHeader.id='victory';
+let yourName='MAHUI'
+newHeader.innerHTML=`${yourName} is the champion`
